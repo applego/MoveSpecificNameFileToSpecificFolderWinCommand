456cfe0b81eac31dd84e37b366f581297ee6d5ad
applego <applegorillappa@gmail.com>
2019-09-09 21:12:21 +0900

    first commit

---
+++ move_file.bat
@@ -0,0 +1,1118 @@
+@echo off
+
+set target=20190411
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190412
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190413
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190414
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190415
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190416
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190417
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190418
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190419
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190420
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190421
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190422
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190423
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190424
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190425
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190426
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190427
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190428
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190429
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190430
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190501
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190502
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190503
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190504
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190505
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190506
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190507
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190508
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190509
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190510
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190511
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190512
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190513
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190514
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190515
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190516
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190517
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190518
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190519
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190520
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190521
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190522
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190523
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190524
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190525
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190526
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190527
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190528
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190529
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190530
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190531
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190601
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190602
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190603
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190604
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190605
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190606
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190607
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190608
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190609
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190610
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190611
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190612
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190613
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190614
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190615
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190616
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190617
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190618
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190619
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190620
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190621
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190622
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190623
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190624
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190625
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190626
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190627
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190628
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190629
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190630
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190701
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190702
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190703
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190704
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190705
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190706
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190707
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190708
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190709
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190710
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190711
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190712
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190713
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190714
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190715
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190716
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190717
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190718
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190719
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190720
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190721
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190722
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190723
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190724
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190725
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190726
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190727
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190728
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190729
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190730
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190731
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190801
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190802
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190803
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190804
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190805
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190806
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190807
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190808
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190809
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190810
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190811
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
+set target=20190812
+
+If not exist %target% mkdir %target%
+
+for %%a in (%target%*.zip) do (
+  echo %%a
+  echo .\%target%\
+  move %%a .\%target%\
+)
